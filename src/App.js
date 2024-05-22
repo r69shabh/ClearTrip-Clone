a7ac2ccf14fe385b1a12a2ec076d0f6d8aebe669
Rishabh Gusain <rishabhgusain.6@gmail.com>
2024-05-22 15:04:56 +0530

    added a HOC, ProtectedRoute to protect pages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Routes, Route, useNavigate } from 'react-router-dom';
+import { Routes, Route, useNavigate, useLocation } from 'react-router-dom';
 import Navbar from './components/Navbar/Navbar'; 
 import Login from './components/Authentication/Login';
 import MyComponent from './components/Authentication/Register';
@@ -7,17 +7,19 @@ import HotelsList from './components/Hotels/Hotelslist';
 import SearchPanel from './components/SearchPanel/SearchPanel';
 import SortComponent from './components/Hotels/SortComponent';
 import Hotel from './components/Hotels/Hotel';
+import ProtectedRoute from './components/Authentication/ProtectedRoute';
 import './App.css';
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   const navigate = useNavigate();
+  const location = useLocation();
 
   useEffect(() => {
-    if (!isLoggedIn) {
+    if (!isLoggedIn && location.pathname !== '/register' && location.pathname !== '/login') {
       navigate('/login');
     }
-  }, [isLoggedIn, navigate]);
+  }, [isLoggedIn, navigate, location]);
 
   const handleLogin = () => {
     setIsLoggedIn(true);
@@ -30,10 +32,28 @@ function App() {
         <Route path="/" element={<Login onLogin={handleLogin}/>} />
         <Route path="/login" element={<Login onLogin={handleLogin} />} />
         <Route path="/register" element={<MyComponent />} />
-        <Route path="/hotels" element={<HotelsList />} />
-        <Route path='/search' element={<SearchPanel />} />
-        <Route path='/sort' element={<SortComponent />} />
-        <Route path='/card' element={<Hotel />} />
+        
+        {/* Protected routes */}
+        <Route path="/hotels" element={
+          <ProtectedRoute isLoggedIn={isLoggedIn}>
+            <HotelsList />
+          </ProtectedRoute>
+        } />
+        <Route path="/search" element={
+          <ProtectedRoute isLoggedIn={isLoggedIn}>
+            <SearchPanel />
+          </ProtectedRoute>
+        } />
+        <Route path="/sort" element={
+          <ProtectedRoute isLoggedIn={isLoggedIn}>
+            <SortComponent />
+          </ProtectedRoute>
+        } />
+        <Route path="/card" element={
+          <ProtectedRoute isLoggedIn={isLoggedIn}>
+            <Hotel />
+          </ProtectedRoute>
+        } />
       </Routes>
     </div>
   );
